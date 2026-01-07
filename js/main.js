bee3b4cb68874f440351e19a133e3f3cfaf85516
Sanika Patil <patilsanika353@gmail.com>
2026-01-07 12:01:17 +0530

    changes added

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
 
-const spans=document.querySelectorAll(`.progress_bar span`);
 
-spans.forEach((span)=>{
-    span.style.width=span.dataset.width;
-    span.innerHTML=span.dataset.width;
+const hamburger = document.getElementById("hamburger");
+const menuList = document.getElementById("menuList");
+
+// toggle menu on hamburger click
+hamburger.addEventListener("click", () => {
+  menuList.classList.toggle("active");
+  hamburger.classList.toggle("open");
 });
 
-
-   
-    //tOGGLE MENU
-function toggleMenu() {
-  document.getElementById("menuList").classList.toggle("active");
-}
+// close menu when any link is clicked
+document.querySelectorAll("#menuList a").forEach(link => {
+  link.addEventListener("click", () => {
+    menuList.classList.remove("active");
+    hamburger.classList.remove("open");
+  });
+});
 
 // emailjs
 document.getElementById("contact-form").addEventListener("submit", function (e) {
@@ -30,6 +34,25 @@ document.getElementById("contact-form").addEventListener("submit", function (e) 
     alert("Failed to send message ");
   });
 });
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("themeToggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains()) {
+      toggleBtn.textContent = "🌙 Dark";
+    } else {
+      toggleBtn.textContent = "☀️ Light ";
+    }
+  });
+});
+
+
 
 
 
