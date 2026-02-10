aa17478d5065dc3d0e22a0050099a2cdfbb972f3
Sanika Patil <patilsanika353@gmail.com>
2026-02-10 23:05:36 +0530

    Add personal portfolio files

---
+++ main.js
@@ -0,0 +1,69 @@
+
+
+const hamburger = document.getElementById("hamburger");
+const menuList = document.getElementById("menuList");
+
+
+
+
+// toggle menu on hamburger click
+hamburger.addEventListener("click", () => {
+  menuList.classList.toggle("active");
+  hamburger.classList.toggle("open");
+});
+
+
+
+
+
+
+// close menu when any link is clicked
+document.querySelectorAll("#menuList a").forEach(link => {
+  link.addEventListener("click", () => {
+    menuList.classList.remove("active");
+    hamburger.classList.remove("open");
+  });
+});
+
+
+
+
+// emailjs
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  emailjs.sendForm(
+    "service_portfolio001",   
+    "template_sanika03",     
+    this
+  )
+  .then(function () {
+    alert("Message sent successfully ");
+    document.getElementById("contact-form").reset();
+  }, function (error) {
+    console.log("FAILED...", error);
+    alert("Failed to send message ");
+  });
+});
+
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const toggleBtn = document.getElementById("themeToggle");
+
+  toggleBtn.addEventListener("click", () => {
+    document.body.classList.toggle("light");
+
+    if (document.body.classList.contains()) {
+      toggleBtn.textContent = "🌙 Dark";
+    } else {
+      toggleBtn.textContent = "☀️ Light ";
+    }
+  });
+});
+
+
+
+
+
